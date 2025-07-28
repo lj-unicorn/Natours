74ae2c9a5dd75c8d1bf6c13b9c5c6e1bb6cce311
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-07-28 22:43:44 +0545

    Add "pre aggregate" middleware and to filter secretTour.

--- models/tourModel.js
+++ models/tourModel.js
@@ -84,6 +84,12 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+
+//NOTE Aggregation Middleware
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
 const Tour = mongoose.model("Tour", tourSchema);
 
 export default Tour;
