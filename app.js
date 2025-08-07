c7eed9b6f6d38e5a7e68d9f5fb8d654a02406a28
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-08-07 08:52:17 +0545

    refactor: integrate global error handler and AppError for unified error response

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import qs from "qs";
 
 import { router as tourRouter } from "./routes/tourRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
+import AppError from "./utils/appError.js";
+import { globalErrorHandler } from "./controllers/errorHandler.js";
 
 export const app = express();
 dotenv.config({ path: "./config.env" });
@@ -23,8 +25,16 @@ app.use("/api/v1/users", userRouter);
 
 //NOTE use regex here
 app.all(/.*/, (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = "fail";
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
