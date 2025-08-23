8b70a8799b377a4015e97bc1db4a87cb20456eb0
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-08-23 22:28:20 +0545

    fix(auth): ensure passwordChangedAt is updated correctly on password change

--- models/userModel.js
+++ models/userModel.js
@@ -59,6 +59,14 @@ userSchema.pre("save", async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+  // Update changedPasswordAt property for the user
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) {
+    return next();
+  }
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
