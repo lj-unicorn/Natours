be745041ba13c209c470ef08791e0d5ffdb68111
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-08-29 19:19:06 +0545

    refactor(app): clean up unused error handling code in app.js

--- app.js
+++ app.js
@@ -25,15 +25,6 @@ app.use("/api/v1/users", userRouter);
 
 //NOTE use regex here
 app.all(/.*/, (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.status = "fail";
-  // err.statusCode = 404;
-
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
