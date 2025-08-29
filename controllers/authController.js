5695bfe3875cdc970a6f4bc0232b39d557a965f9
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-08-29 23:28:58 +0545

    refactor(auth): streamline cookie options setup in createSendToken function

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,23 +12,22 @@ const signToken = (id) => {
   });
 };
 
-// Cookie options setup
-const cookieDays = Number(process.env.JWT_COOKIE_EXPIRES_IN);
-const expiresDate = isNaN(cookieDays)
-  ? new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-  : new Date(Date.now() + cookieDays * 24 * 60 * 60 * 1000);
-
-const cookieOptions = {
-  expires: expiresDate,
-  httpOnly: true,
-};
-
-if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() +
+      (process.env.JWT_COOKIE_EXPIRES_IN ?? 90) * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
   res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: "success",
