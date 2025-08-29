55c5770b8e49e63ff1429974c49385024c0e2c5c
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-08-29 23:52:31 +0545

    refactor(app): reorganize middleware imports and enhance request handling

--- app.js
+++ app.js
@@ -2,29 +2,46 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import qs from "qs";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
 import { router as tourRouter } from "./routes/tourRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
 import AppError from "./utils/appError.js";
 import { globalErrorHandler } from "./controllers/errorHandler.js";
-import rateLimit from "express-rate-limit";
 
 export const app = express();
 dotenv.config({ path: "./config.env" });
 
+// Global Middlewares
+// Set security HTTP header
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
-
 app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
 app.set("query parser", (str) => qs.parse(str));
-app.use(express.json());
+app.use(express.json({ limit: "10kB" }));
+
+// Serving static files
 app.use(express.static(`./public`));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+});
 
 // Routes
 app.use("/api/v1/tours", tourRouter);
