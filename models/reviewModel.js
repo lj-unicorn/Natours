2bd89f13b654c9dad86a2883a8d087f7e05a4081
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-09-05 13:29:47 +0545

    feat(models): add Review model with schema
    - Created reviewModel.js with Mongoose schema
    - Added fields: review (max 500 chars), rating (1–5 with default 0), createdAt (hidden by default)
    - Linked review to User and Tour via ObjectId references
    - Enabled virtuals in toJSON and toObject

---
+++ models/reviewModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      maxLength: [500, "A review should be no more than 500 characters"],
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date().now,
+      select: false,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "A review must be passed by user."],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "A reviw must belong to a tour."],
+    },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+
+export const Review = mongoose.model("Review", reviewSchema);
