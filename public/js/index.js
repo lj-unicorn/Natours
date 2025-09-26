5cf1c9e53bffb20e3b33f7d61b281a28cecc7a45
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-09-27 03:10:23 +0545

    feat(password): implement user password update functionality and refine form handling

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,13 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { login, logout } from "./login.js";
 import { displayMap } from "./map.js";
-import { updateData } from "./updateSettings.js";
+import { updateSettings } from "./updateSettings.js";
 
 const mapElement = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
 const logoutBtn = document.querySelector(".nav__el--logout");
 const userDataForm = document.querySelector(".form-user-data");
+const userPasswordForm = document.querySelector(".form-user-password");
 
 if (mapElement) {
   // Read locations from Pug data attribute
@@ -35,6 +36,25 @@ if (userDataForm) {
     e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
-    updateData(name, email);
+    updateSettings({ name, email }, "data");
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    document.querySelector(".btn--save-password").textContent = "Updating...";
+    const passwordCurrent = document.getElementById("password-current").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("password-confirm").value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      "password",
+    );
+
+    document.getElementById("password-current").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("password-confirm").value = "";
+    document.querySelector(".btn--save-password").textContent = "Save password";
   });
 }
