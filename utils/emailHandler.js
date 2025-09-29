158e9643417dc7ba5b449b8dbcd76844c5614d3f
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-09-30 01:48:36 +0545

    fix(email): correct transporter creation logic for production environment via SendGrid

--- utils/emailHandler.js
+++ utils/emailHandler.js
@@ -14,7 +14,13 @@ export class Email {
   createTransporter() {
     if (process.env.NODE_ENV === "production") {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: "SendGrid",
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        }
+      })
     }
 
     return nodemailer.createTransport({
